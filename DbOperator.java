a303e9d7ffcce9cf250cb784b8f1046786dda15e
StrickerLin <45957193+StrickerLin@users.noreply.github.com>
2020-03-10 16:22:17 +0800

    Create DbOperator.java

---
+++ DbOperator.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+import java.util.*;
+import java.text.*;
+
+public class DbOperator{
+	private String dbName;
+	private String userName;
+	private String password;
+	private Connection conn;
+
+	public DbOperator() {
+		dbName="infect";
+		userName="root";
+		password="123456";
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_demo?useSSL=false&serverTimezone=UTC",userName,password);
+		}catch (ClassNotFoundException e1) {
+			System.out.print(e1);
+			System.exit(1);
+		}catch(SQLException e2){
+			System.out.print(e2);
+			System.exit(2);
+		}
+	}
+
+	public ResultSet getStaticByProvince(String pid,String infectType,java.util.Date d){
+		SimpleDateFormat ft = new SimpleDateFormat ("yyyy-MM-dd");
+		String dStr=ft.format(d);
+		Statement stm;
+		String sqlstr="select "+infectType+"from infect where pid="+pid+" and datadate='"+dStr+"'";
+		try{
+			stm=conn.createStatement();
+			return stm.executeQuery(sqlstr);
+		}catch(SQLException e2){
+			System.out.print("sqlException:"+e2.toString());
+			return null;
+		}
+	}
+
+	public void WriteSql(String pid,String pname,int infect,int dinfect,int cure,int dead,java.util.Date d){
+		
+	}
+}
