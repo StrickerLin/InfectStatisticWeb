903d152165f863c11a2cddb173011fb9e056ca42
linlu <1318486974@qq.com>
2020-03-14 19:13:00 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import dao.*;
 import myUtil.*;
 import sun.security.provider.ConfigFile;
 
+import java.net.ServerSocket;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.List;
@@ -16,49 +17,42 @@ public class Main {
         provinceDAO.insert(Spider.getGovCountryData(website),tableName);
     }
 
-     /*
-    1河北省 2山西省 3辽宁省 4吉林省 5黑龙江省 6江苏省 7浙江省 8安徽省 9福建省 10江西省 11山东省 12河南省 13湖北省 14湖南省 15广东省 16海南省 17四川省 18贵州省
-    19云南省 20陕西省 21甘肃省 22青海省 23台湾省
-自治区：内蒙古自治区 广西壮族自治区 西藏自治区 宁夏回族自治区 新疆维吾尔自治区
-直辖市：北京市 天津市 上海市 重庆市
-特别行政区：香港特别行政区 澳门特别行政区
-     */
-    public static void catchProvinceData(String tableName,List<String> website){
+    public static void catchProvinceData1(List<String> website) {
         ProvinceDAOImpl provinceDAO = new ProvinceDAOImpl();
-        provinceDAO.insert(Spider.getHebeiData(website.get(0)),tableName);
-        provinceDAO.insert(Spider.getShanxiData(website.get(1)),tableName);
-        provinceDAO.insert(Spider.getLiaoNingData(website.get(2)),tableName);
-        provinceDAO.insert(Spider.getJiLinData(website.get(3)),tableName);
-        provinceDAO.insert(Spider.getHeilongjiangData(website.get(4)),tableName);
-        provinceDAO.insert(Spider.getJiangsuData(website.get(5)),tableName);
-        provinceDAO.insert(Spider.getZhejiangData(website.get(6)),tableName);
-        provinceDAO.insert(Spider.getAnhuiData(website.get(7)),tableName);
-        provinceDAO.insert(Spider.getFujianData(website.get(8)),tableName);
-        provinceDAO.insert(Spider.getJiangxiData(website.get(9)),tableName);
-        provinceDAO.insert(Spider.getShandongData(website.get(10)),tableName);
-        provinceDAO.insert(Spider.getHenanData(website.get(11)),tableName);
-        provinceDAO.insert(Spider.getHubeiData(website.get(12)),tableName);
-        provinceDAO.insert(Spider.getHunanData(website.get(13)),tableName);
-        provinceDAO.insert(Spider.getGuangdongData(website.get(14)),tableName);
-        provinceDAO.insert(Spider.getHainanData(website.get(15)),tableName);
-        provinceDAO.insert(Spider.getSichuanData(website.get(16)),tableName);
-        provinceDAO.insert(Spider.getGuizhouData(website.get(17)),tableName);
-        provinceDAO.insert(Spider.getYunnanData(website.get(18)),tableName);
-        provinceDAO.insert(Spider.getShaanxiData(website.get(19)),tableName);
-        provinceDAO.insert(Spider.getGansuData(website.get(20)),tableName);
-        provinceDAO.insert(Spider.getQinghaiData(website.get(21)),tableName);
-        //provinceDAO.insert(Spider.getTaiwanData(website.get(22)),tableName);
-        provinceDAO.insert(Spider.getNeimenguData(website.get(23)),tableName);
-        provinceDAO.insert(Spider.getGuangxiData(website.get(24)),tableName);
-        provinceDAO.insert(Spider.getXizangData(website.get(25)),tableName);
-        provinceDAO.insert(Spider.getNingxiaData(website.get(26)),tableName);
-        provinceDAO.insert(Spider.getXinjiangData(website.get(27)),tableName);
-        provinceDAO.insert(Spider.getBeijingData(website.get(28)),tableName);
-        provinceDAO.insert(Spider.getTianjinData(website.get(29)),tableName);
-        provinceDAO.insert(Spider.getShanghaiData(website.get(30)),tableName);
-        provinceDAO.insert(Spider.getChongqingData(website.get(31)),tableName);
-        //provinceDAO.insert(Spider.getXianggangData(website.get(32)),tableName);
-        //provinceDAO.insert(Spider.getAomenData(website.get(33)),tableName);
+        provinceDAO.insert(Spider.getHebeiData(website.get(0)), "河北");
+        provinceDAO.insert(Spider.getShanxiData(website.get(1)), "山西");
+        provinceDAO.insert(Spider.getLiaoNingData(website.get(2)), "辽宁");
+        provinceDAO.insert(Spider.getJiLinData(website.get(3)), "吉林");
+        provinceDAO.insert(Spider.getHeilongjiangData(website.get(4)), "黑龙江");
+        provinceDAO.insert(Spider.getJiangsuData(website.get(5)), "江苏");
+        provinceDAO.insert(Spider.getZhejiangData(website.get(6)), "浙江");
+        provinceDAO.insert(Spider.getAnhuiData(website.get(7)), "安徽");
+        provinceDAO.insert(Spider.getFujianData(website.get(8)), "福建");
+        provinceDAO.insert(Spider.getJiangxiData(website.get(9)), "江西");
+        provinceDAO.insert(Spider.getShandongData(website.get(10)), "山东");
+        provinceDAO.insert(Spider.getHenanData(website.get(11)), "河南");
+        provinceDAO.insert(Spider.getHubeiData(website.get(12)), "湖北");
+        provinceDAO.insert(Spider.getHunanData(website.get(13)), "湖南");
+        provinceDAO.insert(Spider.getGuangdongData(website.get(14)), "广东");
+        provinceDAO.insert(Spider.getHainanData(website.get(15)), "海南");
+        provinceDAO.insert(Spider.getSichuanData(website.get(16)), "四川");
+        provinceDAO.insert(Spider.getGuizhouData(website.get(17)), "贵州");
+        provinceDAO.insert(Spider.getYunnanData(website.get(18)), "云南");
+        provinceDAO.insert(Spider.getShaanxiData(website.get(19)), "山西");
+        provinceDAO.insert(Spider.getGansuData(website.get(20)), "甘肃");
+        provinceDAO.insert(Spider.getQinghaiData(website.get(21)), "青海");
+        provinceDAO.insert(Spider.getTaiwanData(website.get(22)), "台湾");
+        provinceDAO.insert(Spider.getNeimenguData(website.get(23)), "内蒙古");
+        provinceDAO.insert(Spider.getGuangxiData(website.get(24)), "广西");
+        provinceDAO.insert(Spider.getXizangData(website.get(25)), "西藏");
+        provinceDAO.insert(Spider.getNingxiaData(website.get(26)), "宁夏");
+        provinceDAO.insert(Spider.getXinjiangData(website.get(27)), "新疆");
+        provinceDAO.insert(Spider.getBeijingData(website.get(28)), "北京");
+        provinceDAO.insert(Spider.getTianjinData(website.get(29)), "天津");
+        provinceDAO.insert(Spider.getShanghaiData(website.get(30)), "上海");
+        provinceDAO.insert(Spider.getChongqingData(website.get(31)), "重庆");
+        provinceDAO.insert(Spider.getXianggangData(website.get(32)), "香港");
+        provinceDAO.insert(Spider.getAomenData(website.get(33)), "澳门");
     }
 
     public static  void initData(){
@@ -78,7 +72,7 @@ public class Main {
                 ,"http://wjw.xizang.gov.cn/xwzx/wsjkdt/202003/t20200305_133506.html"
                 ,"http://wsjkw.nx.gov.cn/info/1140/15800.htm","http://www.xjhfpc.gov.cn/info/1495/19542.htm"
                 ,"http://wjw.beijing.gov.cn/xwzx_20031/wnxw/202003/t20200311_1689388.html","http://wsjk.tj.gov.cn/art/2020/3/11/art_87_72162.html"
-                ,"http://wsjkw.sh.gov.cn/xwfb/20200311/391bf753288a4486a06ffa3401f114e9e.html","http://wsjkw.cq.gov.cn/syyqzx/20200311/259361.html");
+                ,"http://wsjkw.sh.gov.cn/xwfb/20200311/391bf753288a4486a06ffa3401f114e9e.html","http://wsjkw.cq.gov.cn/syyqzx/20200311/259361.html","","");
         List<String> data11 = Arrays.asList("http://wsjkw.hebei.gov.cn/content/content_3714/401471.jhtml"
                 ,"http://wjw.shanxi.gov.cn/wjywl02/25235.hrh","http://wsjk.ln.gov.cn/wst_zdzt/xxgzbd/yqtb/202003/t20200312_3769089.html"
                 ,"http://www.jl.gov.cn/szfzt/jlzxd/yqtb/202003/t20200312_6883874.html","http://wsjkw.hlj.gov.cn/index.php/Home/Zwgk/show/newsid/8147/navid/42/stypeid/"
@@ -94,79 +88,50 @@ public class Main {
                 ,"http://wjw.nmg.gov.cn/doc/2020/03/12/296035.shtml","http://wsjkw.gxzf.gov.cn/zhuantiqu/ncov/ncovyqtb/2020/0312/70084.html"
                 ,"http://wjw.xizang.gov.cn/xwzx/wsjkdt/202003/t20200306_133608.html","http://wsjkw.nx.gov.cn/info/1140/15838.htm","http://www.xjhfpc.gov.cn/info/1495/19552.htm"
                 ,"http://wjw.beijing.gov.cn/xwzx_20031/wnxw/202003/t20200312_1697813.html","http://wsjk.tj.gov.cn/art/2020/3/12/art_87_72209.html"
-                ,"http://wsjkw.sh.gov.cn/xwfb/20200312/f8ab81f4929d49a0803749fcfe032d5b.html","http://wsjkw.cq.gov.cn/syyqzx/20200312/259509.html");
+                ,"http://wsjkw.sh.gov.cn/xwfb/20200312/f8ab81f4929d49a0803749fcfe032d5b.html","http://wsjkw.cq.gov.cn/syyqzx/20200312/259509.html","","");
         ProvinceDAOImpl provinceDAO = new ProvinceDAOImpl();
-        provinceDAO.createTable("today");
+        provinceDAO.createTable("latest");
         for (Province province:Spider.getProvincesData()
         ) {
-            provinceDAO.insert(province,"today");
+            //公鸡地图数据
+            provinceDAO.insert(province,"latest");
         }
-        /*catchCountryData("March6","http://www.xinhuanet.com/politics/2020-03/06/c_1125670759.htm");
-        catchCountryData("March7","http://www.xinhuanet.com/politics/2020-03/07/c_1125675222.htm");
-        catchCountryData("March8","http://www.xinhuanet.com/politics/2020-03/08/c_1125679196.htm");
-        catchCountryData("March9","http://www.xinhuanet.com/politics/2020-03/09/c_1125682837.htm");
-        catchCountryData("March10","http://www.xinhuanet.com/politics/2020-03/10/c_1125688175.htm");
-        catchCountryData("March11","http://www.xinhuanet.com/politics/2020-03/11/c_1125694000.htm");*/
-        provinceDAO.createTable("March10");
-        catchCountryData("March10","http://www.xinhuanet.com/politics/2020-03/11/c_1125694000.htm");//全国数据
-        catchProvinceData("March10",data10);
-        provinceDAO.createTable("March11");
-        catchCountryData("March11","http://www.xinhuanet.com/politics/2020-03/12/c_1125699532.htm");//全国数据
-        catchProvinceData("March11",data11);
+        provinceDAO.createTable("country");
+        //全国各种趋势图数据
+        provinceDAO.insert(Spider.getGovCountryData("http://www.xinhuanet.com/politics/2020-03/08/c_1125679196.htm"),"country");//3.07的数据
+        provinceDAO.insert(Spider.getGovCountryData("http://www.xinhuanet.com/politics/2020-03/09/c_1125682837.htm"),"country");//3.08的数据
+        provinceDAO.insert(Spider.getGovCountryData("http://www.xinhuanet.com/politics/2020-03/10/c_1125688175.htm"),"country");//3.09的数据
+        provinceDAO.insert(Spider.getGovCountryData("http://www.xinhuanet.com/politics/2020-03/11/c_1125694000.htm"),"country");//3.10的数据
+        provinceDAO.insert(Spider.getGovCountryData("http://www.xinhuanet.com/politics/2020-03/12/c_1125699532.htm"),"country");//3.11的数据
+        provinceDAO.insert(Spider.getGovCountryData("http://www.xinhuanet.com/politics/2020-03/13/c_1125706520.htm"),"country");//3.12的数据
+        provinceDAO.insert(Spider.getGovCountryData("http://www.xinhuanet.com/politics/2020-03/14/c_1125711265.htm"),"country");//3.13的数据
+        //详细省份数据
+        provinceDAO.createTable("黑龙江");
+        provinceDAO.insert(Spider.getHeilongjiangData("http://wsjkw.hlj.gov.cn/index.php/Home/Zwgk/show/newsid/8142/navid/42/stypeid/"),"黑龙江");//3-7
+        provinceDAO.insert(Spider.getHeilongjiangData("http://wsjkw.hlj.gov.cn/index.php/Home/Zwgk/show/newsid/8144/navid/42/stypeid/"),"黑龙江");//3-8
+        provinceDAO.insert(Spider.getHeilongjiangData("http://wsjkw.hlj.gov.cn/index.php/Home/Zwgk/show/newsid/8145/navid/42/stypeid/"),"黑龙江");//3-9
+        provinceDAO.insert(Spider.getHeilongjiangData("http://wsjkw.hlj.gov.cn/index.php/Home/Zwgk/show/newsid/8146/navid/42/stypeid/"),"黑龙江");//3-10
+        provinceDAO.insert(Spider.getHeilongjiangData("http://wsjkw.hlj.gov.cn/index.php/Home/Zwgk/show/newsid/8147/navid/42/stypeid/"),"黑龙江");//3-11
+        provinceDAO.insert(Spider.getHeilongjiangData("http://wsjkw.hlj.gov.cn/index.php/Home/Zwgk/show/newsid/8148/navid/42/stypeid/"),"黑龙江");//3-12
+        provinceDAO.insert(Spider.getHeilongjiangData("http://wsjkw.hlj.gov.cn/index.php/Home/Zwgk/show/newsid/8149/navid/42/stypeid/"),"黑龙江");//3-13
+
+        String[] names ={"河北", "山西", "辽宁", "吉林", "黑龙江", "江苏", "浙江", "安徽", "福建","江西","山东"
+                , "河南", "湖北", "湖南","广东","海南","四川","贵州","云南" ,"陕西", "甘肃","青海","台湾","内蒙古"
+                , "广西","西藏", "宁夏","新疆","北京","天津","上海", "重庆","香港","澳门"};
+        for (int i=0;i<names.length;i++){
+            provinceDAO.createTable(names[i]);
+        }
+        catchProvinceData1(data10);
+        catchProvinceData1(data11);
+
     }
     public static void main(String[] args) throws SQLException {
         try{
             System.out.println("抓取数据中...");
             initData();
+            System.out.println("数据初始化成功");
         }catch (Exception e){
             e.printStackTrace();
         }
-        ProvinceDAOImpl provinceDAO = new ProvinceDAOImpl();
-        /*provinceDAO.createTable("March12");
-        Spider spider = new Spider();
-        provinceDAO.insert(Spider.getGovCountryData("http://www.xinhuanet.com/politics/2020-03/12/c_1125699532.htm"),"March12");
-        provinceDAO.createTable("March12");*/
-        for (Province province:Spider.getProvincesData()
-             ) {
-            provinceDAO.insert(province,"today");
-        }
-        /*JSONArray array = JSONArray.fromObject(Spider.getAreaStat());
-        JSONObject jsonObject = JSONObject.fromObject(array.getString(0));
-        String provinceName = jsonObject.getString("provinceName");
-        System.out.println("provinceName："+provinceName);//Province country = Json.countryParse(Spider.getCountryStatisticsService());*?
-        //provinceDAO.insert(country);
-        //List<Province> provinces = Json.provinceParse(Spider.getProvinceStatisticService());
-        /*for (Province province: provinces
-             ) {
-            provinceDAO.insert(province);
-        }
-        for (Province province:provinceDAO.list()
-             ) {
-            System.out.println(province.getName()+' '+province.getDay()+' '+province.getIp()+' '+ province.getSp()+' '+province.getCure()+' '+province.getDead());
-        }
-        //Spider spider = new Spider();
-        spider.initWebsites();
-        for (String website:spider.getWebsites()
-             ) {
-            provinceDAO.insert(Spider.getGovCountryData(website));
-        }*/
-
-
-        for (Province province:provinceDAO.list("March10")
-        ) {
-            province.printString();
-        }
-        for (Province province:provinceDAO.list("March11")
-        ) {
-            province.printString();
-        }
-        /*
-        for (Province province:provinceDAO.list("today")) {
-            province.printString();
-        }*/
-        //System.out.println(Spider.getAreaStat());
-       // Spider.getGovCountryData("http://www.xinhuanet.com/politics/2020-03/12/c_1125699532.htm").printString();
     }
-
-
 }
